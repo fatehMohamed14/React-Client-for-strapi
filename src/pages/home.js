38b278065b1ae0f6dbe8abd001f884c94267fd07
fateh mohamed <cr7141414@gmail.com>
2021-01-21 18:18:18 +0100

    add pagination

--- src/pages/home.js
+++ src/pages/home.js
@@ -19,21 +19,34 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const getRequestParams = (start) => {
+  console.log(start)
+  let params = {};
+  params["_start"] = (start-1)*2;
+  params["_limit"] = 2;
+  return params;
+};
+
 export default () => {
 
   const [posts, setPosts] = useState([]);
+  const [page, setPage] = React.useState(1);
   const classes = useStyles();
   useEffect(
     () => {
+      const params = getRequestParams(page);
       const getPosts = async () => {
-        const response = await API.get('/articles?_start=0&_limit=10');
+        const response = await API.get('/articles', { params });
         console.log(response)
         setPosts(response.data)
       }
       getPosts();
 
-    }, []
+    }, [page]
   )
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
 
   return (
     <Grid container justify="center" className={classes.root} spacing={2} >
@@ -41,7 +54,7 @@ export default () => {
       {posts.map(post => <Grid item key={post.id}  ><Post className={classes.paper} url={post.thumbnail && post.thumbnail.url} title={post.title} likes={post.likes} description={post.content} /></Grid>)}
       <Grid container justify="center" >
         <Box m="5rem">
-          <Pagination count={5}  color="primary" />
+          <Pagination count={5} color="primary" page={page} onChange={handlePageChange} />
         </Box>
       </Grid>
     </Grid>
