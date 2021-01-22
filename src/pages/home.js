2565df871d94866b157a21ea37a2ef92bbec09d0
fateh mohamed <cr7141414@gmail.com>
2021-01-22 16:49:30 +0100

    fix pagination bug

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import Pagination from '@material-ui/lab/Pagination'
 import Box from '@material-ui/core/Box';
 
+const pageSize = 10;
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -20,25 +21,25 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const getRequestParams = (start) => {
-  console.log(start)
   let params = {};
-  params["_start"] = (start-1)*2;
-  params["_limit"] = 2;
+  params["_start"] = (start-1)*pageSize;
+  params["_limit"] = pageSize;
   return params;
 };
 
 export default () => {
 
   const [posts, setPosts] = useState([]);
-  const [page, setPage] = React.useState(1);
+  const [page, setPage] = useState(1);
+  const [count, setCount] = useState(0);
   const classes = useStyles();
   useEffect(
     () => {
       const params = getRequestParams(page);
       const getPosts = async () => {
         const response = await API.get('/articles', { params });
-        console.log(response)
-        setPosts(response.data)
+        setPosts(response.data.items)
+        setCount(response.data.count)
       }
       getPosts();
 
@@ -54,7 +55,7 @@ export default () => {
       {posts.map(post => <Grid item key={post.id}  ><Post className={classes.paper} url={post.thumbnail && post.thumbnail.url} title={post.title} likes={post.likes} description={post.content} /></Grid>)}
       <Grid container justify="center" >
         <Box m="5rem">
-          <Pagination count={5} color="primary" page={page} onChange={handlePageChange} />
+          <Pagination count={Math.ceil(count/pageSize)} color="primary" page={page} onChange={handlePageChange} />
         </Box>
       </Grid>
     </Grid>
